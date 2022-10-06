56198f56fb9dd12cd82307b1f3ddfc457c8de29a
Joshua Lee <joshua.lee@student.allenisd.org>
2022-10-06 15:14:37 -0500

    initial commit

---
+++ main.swift
@@ -0,0 +1,30 @@
+// Add your code below:
+var unsortedArray = [String]()
+while let line = readLine() {
+    unsortedArray.append(line)
+}
+
+func swap(array: inout [String], firstIndex: Int, secondIndex: Int) {
+    let swaps = array[firstIndex]
+    array[firstIndex] = array[secondIndex]
+    array[secondIndex] = swaps
+}
+
+func selectionSort(array: inout [String]) {
+    
+    for i in 0..<array.count {
+        var min = i
+        for x in i + 1..<array.count {
+            if array[x] < array[min] {
+                min = x
+                
+            }
+        }
+        
+        swap(array: &array, firstIndex: i, secondIndex: min)
+        
+    }
+    print(array)
+}
+
+selectionSort(array: &unsortedArray)
